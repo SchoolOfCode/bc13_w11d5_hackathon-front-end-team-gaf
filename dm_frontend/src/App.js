5c231001671c9164b750256235cf25cbc8745225
Barbara Millon <millon.barbara1@gmail.com>
2022-12-09 12:30:48 +0000

    pre lunch changes

--- dm_frontend/src/App.js
+++ dm_frontend/src/App.js
@@ -1,21 +1,45 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
+import 
 
+
+const convertToAnswer = (dto) => {
+  return {
+    id: dto.id,
+    answer: dto.answer}
+}
 
 function App() {
   const [answer, setAnswer] = useState('')
+  const [answers, setAnswers] = useState([])
 
-  const getAnswer = () => {
-    fetch('http://localhost:3001/answer')
-      .then(response => response.json())
-      .then(data => setAnswer(data))
+
+  useEffect(() => {
+    fetch('http://localhost:3001/answers', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+        }
+        })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          setAnswers(data)
+        })
+        .catch(error => console.log(error))
+  }, [])
+
+  
+
+
+
+  function getRandomAnswer(){
+    
+    const randomAnswer =  answers[Math.floor(Math.random() * answers.length)]; 
+    setAnswer(randomAnswer);
+    console.log(randomAnswer);
   }
-
-  const getRandomAnswer = answers[Math.floor(Math.random() * answers.length)]
-
-
-
 
 
 
@@ -24,10 +48,7 @@ function App() {
     <div className="App">
       <h1>Decision maker</h1>
       <h3>Focus on a question, then click the button below for an answer</h3>
-      <button>Get answer</button>
-
-
-
+      <button onClick= {() => {getRandomAnswer()}}>Get answer</button>
     </div>
   );
 }
