0803f338315ca154af5a0f17c8d54babd0427eb0
Barbara Millon <millon.barbara1@gmail.com>
2022-12-09 15:11:12 +0000

    front end almost done

--- dm_frontend/src/App.js
+++ dm_frontend/src/App.js
@@ -1,56 +1,83 @@
 import './App.css';
-import React, { useEffect } from 'react';
-import { useState } from 'react';
-import 
+import React, { useState } from 'react';
+import logo from './logo.png';
 
 
-const convertToAnswer = (dto) => {
-  return {
-    id: dto.id,
-    answer: dto.answer}
-}
+const answers = [
+  {
+    answer: 'Yes'
+  },
+  {
+    answer: 'No'
+  },
+  {
+    answer: 'Maybe'
+  },
+  {
+    answer: 'Ask again later'
+  },
+  {
+    answer: 'It is certain'
+  },
+  {
+    answer: 'It is decidedly so'
+  },
+  {
+    answer: 'Without a doubt'
+  },
+]
+
 
 function App() {
   const [answer, setAnswer] = useState('')
-  const [answers, setAnswers] = useState([])
-
-
-  useEffect(() => {
-    fetch('http://localhost:3001/answers', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-        }
-        })
-        .then(response => response.json())
-        .then(data => {
-          console.log(data)
-          setAnswers(data)
-        })
-        .catch(error => console.log(error))
-  }, [])
 
   
+  // useEffect(() => {
+  //   fetch('http://localhost:3001/answers', {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     }
+  //       .then(response => response.json())
+  //       .then(data => {
+  //         const answers = data.map(answer => {
+  //           return {
+  //             answer: answer.answer
+  //           }
+  //         })
+  //         setAnswers(answers)
+  //         const randomAnswer = answers[Math.floor(Math.random() * answers.length)]
+  //         setAnswer(randomAnswer.answer)
+  //         console.log(randomAnswer.answer)
+  //       }
+  //       )
+  //   }
 
+  //   )
+  // });
 
 
   function getRandomAnswer(){
-    
-    const randomAnswer =  answers[Math.floor(Math.random() * answers.length)]; 
-    setAnswer(randomAnswer);
-    console.log(randomAnswer);
+    const randomAnswer = answers[Math.floor(Math.random() * answers.length)]
+    setAnswer(randomAnswer.answer)
+    console.log(randomAnswer.answer)
   }
+      
 
 
 
 
   return (
     <div className="App">
-      <h1>Decision maker</h1>
+      <img className="logo" src={logo} alt="logo" />     
+      <h1>Decision Maker</h1>
       <h3>Focus on a question, then click the button below for an answer</h3>
-      <button onClick= {() => {getRandomAnswer()}}>Get answer</button>
+      <button onClick = {getRandomAnswer} >Get Answer</button>
+      <h2> { answer }</h2>
+
     </div>
   );
 }
+
 
 export default App;
