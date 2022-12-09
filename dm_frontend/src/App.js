ecbfa07bd04b77868ebbcd8f2d65a6fd3303d30b
Barbara Millon <millon.barbara1@gmail.com>
2022-12-09 21:47:00 +0000

    removed getAnswe() from useEffect, which caused an answer to appear before the button was even clicked

--- dm_frontend/src/App.js
+++ dm_frontend/src/App.js
@@ -3,15 +3,9 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.png';
 
 
-// const convertToAnswer = (dto) => {
-// 	return { id: dto.id, answer: dto.answer};
-// };
-
-
-
 function App() {
-  // const [answer, setAnswer] = useState('')
   const [answer, setAnswer] = useState([])
+
 
   const getAnswer = async () => {
     const response = await fetch("http://localhost:3001/answer", {
@@ -29,16 +23,9 @@ function App() {
 
   useEffect(() => {
 
-    
-    getAnswer();
   }, []);
 
 
-
-
-  function handleClick () {
-    const answers = answer;
-  }
 
 
 
@@ -47,7 +34,8 @@ function App() {
       <img className="logo" src={logo} alt="logo" />     
       <h1>Decision Maker</h1>
       <h3>Focus on a question, then click the button below for an answer</h3>
-      <button onClick={() => getAnswer()}>Get Answer</button>
+      <button onClick={() => getAnswer()} className="getAnswerButton">Get Answer</button>
+
       <h2>{answer}</h2>
     </div>
   );
